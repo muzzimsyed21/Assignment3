c907aa14cc86b53a28b3e7e408c0a4a20a3c0d97
Muzamil <muzzimsyed21@gmail.com>
2015-05-10 20:09:44 -0700

    Created Indexer Class/ Added Library

---
+++ src/Indexer/Indexer.java
@@ -0,0 +1,16 @@
+package Indexer; 
+
+import org.codehaus.jackson.JsonParser; 
+
+public class Indexer {
+	
+	public static void createIndex(String crawledPagesLocation){
+		
+		//takes as parameter the location of the crawled dump provided by TA 
+		//put the location of crawled data somewhere other the location of 
+		//of code that you will push to repository since its prob not a good idea to 
+		//push a file that large to github  
+		
+	}
+
+}
