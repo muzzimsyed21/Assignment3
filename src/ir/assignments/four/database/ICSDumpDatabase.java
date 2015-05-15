a261fe4200189ed365d73385f290f7478e720a3b
jonbudi <jonbudi1@gmail.com>
2015-05-15 09:41:53 -0700

    Code clean up

--- src/ir/assignments/four/database/ICSDumpDatabase.java
+++ src/ir/assignments/four/database/ICSDumpDatabase.java
@@ -4,125 +4,104 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Properties;
-import java.util.Map;
-import java.util.Scanner;
 
 public class ICSDumpDatabase {
-	
-	private String username; 
-	private String password; 	
+
+	private String username;
+	private String password;
 	private String databaseName = null;
-	private Connection connection = null; 
+	private Connection connection = null;
 
 	public ICSDumpDatabase(String username, String password, String databaseName) {
 		this.username = username;
-		this.password = password; 
+		this.password = password;
 		this.databaseName = databaseName;
 	}
 
 	public void create() {
-
 		try {
-			 
 			initDb();
-			setConnectionAfterDatabaseCreation(); 
-			initTermToTermIdTable(); 
-			initDocIdToTermIdTable(); 
-			initDocIDToUrlIdTable();
-			
-
+			setConnectionAfterDatabaseCreation();
+			initTermToTermIdTable();
+			initDocIdToTermIdTable();
+			initDocIdToUrlIdTable();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			
 			//e.printStackTrace();
 		}
-
 	}
-	
-	private void initDb(){
-		
+
+	private void initDb() {
 		try {
-			
-			Connection connection = getConnection();
-			String qCreateDb = String.format("create database if not exists %s;", this.databaseName);
+			Connection connection = getInitialConnection();
+			String qCreateDb = String.format("CREATE DATABASE IF NOT EXISTS %s;", this.databaseName);
 			PreparedStatement statement = connection.prepareStatement(qCreateDb);
 			statement.executeUpdate();
 			connection.close();
 			System.out.println("Initialized database");
-			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 		
-		
+		}
 
 	}
-	
 
 	private void initTermToTermIdTable() throws SQLException {
-		
 		PreparedStatement statement;
+		
 		String qCreateTblTerms = "CREATE TABLE IF NOT EXISTS icsdump.termtotermid "
-				+ "(termid INT NOT NULL,"
-				+ "term VARCHAR(64),"
-				+ "PRIMARY KEY (termid));"; 	
+				+ "(termid INT NOT NULL," + "term VARCHAR(64)," + "PRIMARY KEY (termid));";
+		
 		statement = this.connection.prepareStatement(qCreateTblTerms);
 		statement.executeUpdate();
 		System.out.println("Initialized termToTermId table");
-		
 	}
 
 	private void initDocIdToTermIdTable() throws SQLException {
-
 		PreparedStatement statement;
-		
+
 		String qCreateTblTerms = "CREATE TABLE IF NOT EXISTS icsdump.docidtotermid "
-				+ "(docid INT NOT NULL,"
-				+ "termid INT,"
-				+ "PRIMARY KEY (docid),"
-				+ "FOREIGN KEY (termid) REFERENCES termtotermid(termid));"; 
-		
+				+ "(docid INT NOT NULL," + "termid INT," + "PRIMARY KEY (docid),"
+				+ "FOREIGN KEY (termid) REFERENCES termtotermid(termid));";
+
 		statement = this.connection.prepareStatement(qCreateTblTerms);
 		statement.executeUpdate();
 		System.out.println("Initialized DocIdToTermId table");
-		
+
 	}
 
-	private void initDocIDToUrlIdTable() throws SQLException {
-
+	private void initDocIdToUrlIdTable() throws SQLException {
 		PreparedStatement statement;
-		
+
 		String qCreateTblTerms = "CREATE TABLE IF NOT EXISTS icsdump.docidtourlid "
-				+ "(urlid INT NOT NULL,"
-				+ "docid INT,"
-				+ "PRIMARY KEY (urlid),"
-				+ "FOREIGN KEY (docid) REFERENCES docidtotermid(docid));"; 
-		
-		
+				+ "(urlid INT NOT NULL," + "docid INT," + "PRIMARY KEY (urlid),"
+				+ "FOREIGN KEY (docid) REFERENCES docidtotermid(docid));";
+
 		statement = this.connection.prepareStatement(qCreateTblTerms);
 		statement.executeUpdate();
 		System.out.println("Initialized DocIdToUrlId table");
 	}
-	
+
 	//create insert functions
-	
-	private Connection getConnection() throws SQLException{
-		
-		
-		Connection connection= DriverManager.getConnection("jdbc:mysql://localhost/?user=" 
-		+this.username + "&password=" + this.password);
-		
-		return connection; 
+
+	private Connection getInitialConnection() throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/?user="
+				+ this.username + "&password=" + this.password);
+		return connection;
+	}
+
+	private void setConnectionAfterDatabaseCreation() throws SQLException {
+		this.connection = DriverManager.getConnection("jdbc:mysql://localhost/" + this.databaseName
+				+ "?user=" + this.username + "&password=" + this.password);
 	}
 	
-	private void setConnectionAfterDatabaseCreation() throws SQLException{
-		
-		
-		this.connection= DriverManager.getConnection("jdbc:mysql://localhost/"+ this.databaseName 
-				+ "?user=" + this.username + "&password=" + this.password);
+	/** close connection object **/
+	public void close() {
+		try {
+			if (this.connection != null) {
+				this.connection.close();
+			}
+		} catch (SQLException e) {
+		}
+		this.connection = null;
 	}
 
 }
--- src/ir/assignments/four/database/LoadDatabase.java
+++ src/ir/assignments/four/database/LoadDatabase.java
@@ -1,31 +1,19 @@
 package ir.assignments.four.database;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-
-import ir.assignments.four.indexer.CreateIndex;
-import ir.assignments.four.indexer.IndexerLocations;
-import ir.assignments.four.indexer.LoadIndex; 
-import ir.assignments.four.util.Util;
-
 public class LoadDatabase {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//List<File> files = Util.getFilesInPath(IndexerLocations.fileDump);
 
 		//construct and initiate database
-		ICSDumpDatabase termDatabase = new ICSDumpDatabase("root", "Password1", "ICSDump"); 
+		ICSDumpDatabase termDatabase = new ICSDumpDatabase("root", "Password1", "ICSDump");
 		termDatabase.create(); //database name => "ICSDump" call this only once!
-		
+		termDatabase.close();
 		//store termToTermId (both ways)
-		
+
 		//store DocIdToTermId (both ways)
-		
+
 		//store DocIDToUrl (both ways) 
-		
-	
 
 	}
 
