bb695d6ea445369719e9dd567b313c60aaa3ef38
Muzamil <muzzimsyed21@gmail.com>
2015-05-23 14:53:48 -0700

    PageRank class?

--- src/ir/assignments/four/pageRank/PageRank.java
+++ src/ir/assignments/four/pageRank/PageRank.java
@@ -1,13 +1,30 @@
 package ir.assignments.four.pageRank;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 import ir.assignments.four.domain.FileDumpObject;
 
 public class PageRank {
 	public static FileDumpObject file; 
+	public Map<String, Integer> outNodes;
+	public Map<String, Integer> inNodes;
 	
 	public PageRank(){
+		this.outNodes = new TreeMap<String, Integer>();
+		this.inNodes = new TreeMap<String, Integer>();
 		
+	}
+	
+	public Map<String, Integer> getOutNodeMaps(String url){
 		
+		return this.outNodes; 
+		
+	}
+	
+	public Map<String, Integer> getInNodes(String url){
+		
+		return this.inNodes; 
 	}
 
 	public static void main(String[] args) {
