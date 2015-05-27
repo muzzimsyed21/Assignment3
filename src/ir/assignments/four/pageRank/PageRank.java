5b6915ca2522a679b2412fa987e96e1930bf4d3b
Muzamil <muzzimsyed21@gmail.com>
2015-05-26 17:03:13 -0700

    page rank template

--- src/ir/assignments/four/pageRank/PageRank.java
+++ src/ir/assignments/four/pageRank/PageRank.java
@@ -1,28 +1,31 @@
 package ir.assignments.four.pageRank;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import ir.assignments.four.domain.FileDumpObject;
 
 public class PageRank {
 	public static FileDumpObject file; 
-	public Map<String, Integer> outNodes;
-	public Map<String, Integer> inNodes;
+	public Map<Integer,
+	Set<Integer>> outNodes;
+	public Map<Integer, Set<Integer>> inNodes;
 	
 	public PageRank(){
-		this.outNodes = new TreeMap<String, Integer>();
-		this.inNodes = new TreeMap<String, Integer>();
+		this.outNodes = new TreeMap<Integer, Set<Integer>>();
+		this.inNodes = new TreeMap<Integer, Set<Integer>>();
 		
 	}
 	
-	public Map<String, Integer> getOutNodeMaps(String url){
+	public  Map<Integer, Set<Integer>> getOutNodeMaps(Map<String, Integer> docIdToUrlMap){
 		
 		return this.outNodes; 
 		
 	}
 	
-	public Map<String, Integer> getInNodes(String url){
+	public Map<Integer, Set<Integer>> getInNodes(){
 		
 		return this.inNodes; 
 	}
