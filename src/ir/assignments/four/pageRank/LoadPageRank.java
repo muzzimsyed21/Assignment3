59bf5a28581c695acbdfe04bde245666f13eb028
jonbudi <jonbudi1@gmail.com>
2015-06-02 12:48:28 -0700

    PageRank save and load complete

--- src/ir/assignments/four/pageRank/LoadPageRank.java
+++ src/ir/assignments/four/pageRank/LoadPageRank.java
@@ -5,13 +5,15 @@ import ir.assignments.four.util.Util;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class LoadPageRank {
 	
 	/** load node map **/
-	public static Map<String, Integer> loadNodeMap(String path) {
-		Map<String, Integer> map = new TreeMap<String, Integer>();
+	public static Map<Integer, Set<Integer>> loadNodeMap(String path) {
+		Map<Integer, Set<Integer>> map = new TreeMap<Integer, Set<Integer>>();
 
 		String fileString = null;
 		try {
@@ -19,10 +21,46 @@ public class LoadPageRank {
 		} catch (IOException e) {
 		}
 
-		int index = 0;
+		int docId = 0;
 		if (fileString != null) {
-			for (String term : Util.tokenize(fileString)) {
-				map.put(term, index++);
+			boolean first;
+			for (String split : fileString.split("\n")) {
+				first = true;
+				for (String termId : Util.tokenize(split)) {
+					if (first) {
+						docId = Integer.parseInt(termId);
+						map.put(docId, new TreeSet<Integer>());
+						first = false;
+					} else {
+						map.get(docId).add(Integer.parseInt(termId));
+					}
+				}
+			}
+		}
+
+		return map;
+	}
+	
+	/** load page rank scores map **/
+	public static Map<Integer, Double> loadPageRankScoresMap(String path) {
+		Map<Integer, Double> map = new TreeMap<Integer, Double>();
+
+		String fileString = null;
+		try {
+			fileString = Util.readFile(new File(path));
+		} catch (IOException e) {
+		}
+
+		int docID = 0;
+		if (fileString != null) {
+			boolean first = true;
+			for (String score : Util.tokenize(fileString)) {
+				if (first) {
+					map.put(docID++, Double.parseDouble(score));
+					first = false;
+				} else {
+					first = true;
+				}
 			}
 		}
 
--- src/ir/assignments/four/pageRank/PageRank.java
+++ src/ir/assignments/four/pageRank/PageRank.java
@@ -18,22 +18,22 @@ import org.json.JSONException;
 public class PageRank {
 
 	/** toggle true to recreate page rank **/
-	private final static boolean CREATEFLAG = true;
+	private final static boolean CREATEFLAG = false;
 
 	public static Map<Integer, Set<Integer>> outNodes;
 	public static Map<Integer, Set<Integer>> inNodes;
 	public static Map<Integer, Double> pageRankScores;
 
 	public static void init() {
+		outNodes = new TreeMap<Integer, Set<Integer>>();
+		inNodes = new TreeMap<Integer, Set<Integer>>();
+		pageRankScores = new TreeMap<Integer, Double>();
+		
 		if (CREATEFLAG) {
 			List<File> files = Util.getFilesInPath(IndexerLocations.fileDump);
 
 			Map<Integer, String> docIdToUrlMap = LoadIndex.loadDocIdToUrlMap(IndexerLocations.docIdToUrlCSV);
 			Map<String, Integer> urlToDocIdMap = LoadIndex.loadUrlToDocIdMap(docIdToUrlMap);
-
-			outNodes = new TreeMap<Integer, Set<Integer>>();
-			inNodes = new TreeMap<Integer, Set<Integer>>();
-			pageRankScores = new TreeMap<Integer, Double>();
 
 			for (int i = 0; i < docIdToUrlMap.size(); ++i) {
 				outNodes.put(i, new TreeSet<Integer>());
@@ -41,6 +41,7 @@ public class PageRank {
 				pageRankScores.put(i, 0.0);
 			}
 
+			int count = 0;
 			for (File file : files) {
 				FileDumpObject fdo = null;
 				try {
@@ -57,6 +58,7 @@ public class PageRank {
 					}
 				}
 				fdo = null;
+				System.out.println(count++);
 			}
 
 			// create inNodes map
@@ -79,8 +81,18 @@ public class PageRank {
 			SavePageRank.saveNodesMap(inNodes, PageRankLocations.inNodesCSV);
 			SavePageRank.savePageRankScoresMap(pageRankScores, PageRankLocations.pageRankCSV);
 		} else {
-			
+			outNodes = LoadPageRank.loadNodeMap(PageRankLocations.outNodesCSV);
+			inNodes = LoadPageRank.loadNodeMap(PageRankLocations.inNodesCSV);
+			pageRankScores = LoadPageRank.loadPageRankScoresMap(PageRankLocations.pageRankCSV);
 		}
+		
+		System.out.println(outNodes.size());
+		System.out.println(inNodes.size());
+		System.out.println(pageRankScores.size());
+		
+		System.out.println(outNodes.get(0));
+		System.out.println(inNodes.get(0));
+		System.out.println(pageRankScores.get(0));
 	}
 
 	public static Set<Integer> getOutNodes(int docId) {
